329bd2974a649c58fb3eac17550f2b3ee882a684
estebancasalas <e.casalas@outlook.com>
2021-09-06 10:59:11 -0300

    Hecho lector y tablero

--- src/Library/Lector.cs
+++ src/Library/Lector.cs
@@ -1,8 +1,27 @@
 ﻿using System;
+using System.IO;
 
 namespace PII_Game_Of_Life
 {
     public class Lector
     {
+        public static bool[,] Leer(string url)
+        {
+        //string url = ruta;
+        string content = File.ReadAllText(url);
+        string[] contentLines = content.Split('\n');
+        bool[,] Board = new bool[contentLines.Length, contentLines[0].Length];
+        for (int  y=0; y<contentLines.Length;y++)
+        {
+            for (int x=0; x<contentLines[y].Length; x++)
+            {
+                if(contentLines[y][x]=='1')
+                {
+                    Board[x,y]=true;
+                }
+            }
+        }
+        return Board;
+        }
     }
 }
--- src/Library/Tablero.cs
+++ src/Library/Tablero.cs
@@ -3,7 +3,65 @@ using System;
 namespace PII_Game_Of_Life
 {
     public class Tablero
-    {
+    {   
+        private bool[,] gameBoard;
+        private int boardWidth;
+        private int boardHeight;
+
+        public Tablero(bool[,] board)
+        {
+            
+            this.gameBoard = board;
+        }
+    
+        public void Ciclo()
+        {
+            bool[,] cloneboard = new bool[this.boardWidth, this.boardHeight];
+            for (int x = 0; x < boardWidth; x++)
+            {
+                for (int y = 0; y < boardHeight; y++)
+                {
+                    int aliveNeighbors = 0;
+                    for (int i = x-1; i<=x+1;i++)
+                    {
+                        for (int j = y-1;j<=y+1;j++)
+                        {
+                            if(i>=0 && i<boardWidth && j>=0 && j < boardHeight && this.gameBoard[i,j])
+                            {
+                                aliveNeighbors++;
+                            }
+                        }
+                    }
+                    if(this.gameBoard[x,y])
+                    {
+                        aliveNeighbors--;
+                    }
+                    if (this.gameBoard[x,y] && aliveNeighbors < 2)
+                    {
+                        //Celula muere por baja población
+                        cloneboard[x,y] = false;
+                    }
+                    else if (this.gameBoard[x,y] && aliveNeighbors > 3)
+                    {
+                        //Celula muere por sobrepoblación
+                        cloneboard[x,y] = false;
+                    }
+                    else if (!this.gameBoard[x,y] && aliveNeighbors == 3)
+                    {
+                        //Celula nace por reproducción
+                        cloneboard[x,y] = true;
+                    }
+                    else
+                    {
+                        //Celula mantiene el estado que tenía
+                        cloneboard[x,y] = this.gameBoard[x,y];
+                    }
+                }
+            }
+            this.gameBoard = cloneboard;
+            cloneboard = new bool[boardWidth, boardHeight];
+        }
+
         
     }
 }
